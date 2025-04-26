64901d3a0b120b0aa26492edf6e738a80484a94c
Ruslan Kolomenskiy <charly100@yandex.ru>
2025-04-26 11:45:57 +0300

    Lab6

--- STLLearning/STLLearning.cpp
+++ STLLearning/STLLearning.cpp
@@ -6,194 +6,72 @@
 #include <map>
 #include <algorithm>
 #include <set>
+#include <list>
 #include <vector>
 
 
 
-enum BrainLevel
+
+int gen()
 {
-	DOOP,
-	FOOL,
-	AVERAGE,
-	SMART,
-	BRAINIAC
-
-};
-
-class Character
-{
-public:
-	Character(const std::string name, int cool, BrainLevel level)
-	{
-		this->Name = name;
-		this->Cool = cool;
-		this->Intelligence = level;
-	}
-
-	Character(const Character& copy)
-	{
-		this->Name = copy.Name;
-		this->Cool = copy.Cool;
-		this->Intelligence = copy.Intelligence;
-	}
-
-
-	int GetCool() const
-	{
-		return Cool;
-	}
-
-	std::string GetName() const
-	{
-		return Name;
-	}
-
-	bool operator>(const Character& other )
-	{
-		return this->Cool > other.GetCool();
-	}
-
-	BrainLevel GetBrainLEvel() const
-	{
-		return Intelligence;
-	}
-private:
-
-
-private :
-	std::string Name;
-	int Cool;
-	BrainLevel Intelligence;
-	
-
+	return rand();
 };
 
 
-
-
-
-void Print(const std::pair<std::string, Character>& chr)
-{
-	std::cout << chr.first << '\n';
-}
-
-
-class Fighter
-{
-	using Str = std::string;
-public:
-	Fighter(Str name, Str First, Str Second, Str Third, Str Fourth) : Name(name)
-	{
-		this->PowerLevels = new Str[4];
-		PowerLevels[0] = First;
-		PowerLevels[1] = Second;
-		PowerLevels[2] = Third;
-		PowerLevels[3] = Fourth;
-	}
-		
-	Fighter(const Fighter& copy)
-	{
-		Name = copy.Name;
-		std::cout << "copy constunctor called for " << Name <<'\n';
-		this->PowerLevels = new Str[4];
-		for (int i = 0; i < 4; i++)
-		{
-			this->PowerLevels[i] = copy.PowerLevels[i];
-		}
-	
-		 
-	}
-
-	
-	Fighter& operator=(const Fighter& copy) 
-	{
-		delete[] this->PowerLevels;
-		Name = copy.Name;
-		std::cout << "copy constunctor called for " << Name << '\n';
-		this->PowerLevels = new Str[4];
-		for (int i = 0; i < 4; i++)
-		{
-			this->PowerLevels[i] = copy.PowerLevels[i];
-		}
-		
-		return *this;
-	}
-
-	Str PrintName()
-	{
-		return Name;
-	}
-private:
-	Str Name;
-	std::string* PowerLevels;
-};
 
 int main()
 {
 
 
-	// copy algorithm for unordered_map 
+	
+	//generate algorithm
 
-	std::cout << " Copy unordered_map\n";
+	std::cout << "My list is : \n";
 
-	std::unordered_map<std::string, Character> SeriesU;
+	std::list<int> MyList(10);
 
-	std::unordered_map<std::string, Character> SeriesCoolCharacters;
+	std::generate(MyList.begin(), MyList.end(), gen);
 
-
-	SeriesU.emplace("Morty", Character("Morty", 120, BrainLevel::FOOL));
-	SeriesU.emplace("Summer", Character("Summer", 320, BrainLevel::SMART));
-	SeriesU.emplace("Rick", Character("Rick", 1000, BrainLevel::BRAINIAC));
-	SeriesU.emplace("Beth", Character("Beth", 680, BrainLevel::SMART));
-	SeriesU.emplace("EvilMorty", Character("EvilMorty", 1000, BrainLevel::BRAINIAC));
-	SeriesU.emplace("BirdPerson", Character("BirdPerson", 830, BrainLevel::SMART));
-	SeriesU.emplace("GearHead", Character("GearHead", 70, BrainLevel::AVERAGE));
-
-	Character PoopyButtHole("PoppyButtHole", 500, BrainLevel::AVERAGE); 
-
-	copy_if(SeriesU.begin(), SeriesU.end(), std::inserter(SeriesCoolCharacters, SeriesCoolCharacters.begin()), [PoopyButtHole](const std::pair<std::string, Character>& chr) {
+	for (auto i : MyList)
+	{
+		std::cout << i << '\n';
 		
-		return chr.second.GetCool() > PoopyButtHole.GetCool() ;
+	}
+
+	std::list<int> NewList(10);
+
+	// transform alorithm
+
+	std::cout << "A new list is : \n";
+
+	std::transform(MyList.cbegin(), MyList.cend(), NewList.begin(), [](int x) {
+		
+		return x % 10;
 		});
 
-	std::cout << "These are cool characters : \n";
 
-	std::unordered_map<std::string, Character>::const_iterator It;
-	It = SeriesCoolCharacters.begin();
-	for_each(SeriesCoolCharacters.begin(), SeriesCoolCharacters.end(), Print);
-	
-	
-	// deep copy
 
-	std::cout << "\n Deep Copy \n ";
-
-	std::vector<Fighter> DB =
+	for (auto i : NewList)
 	{
-		Fighter("Goku", "SSJ1","SSJ2", "SSJ3", "SSBlue"),
-		Fighter("Vegeta", "SSJ1","SSJ2", "SSJ3", "SSBlue")
-	};
+		std::cout << i << '\n';
 
-	std::vector<Fighter> DBdouble;
-	
+	}
 
-	copy(DB.cbegin(), DB.cend(), back_inserter(DBdouble));
-	std::cout << "\nAfter coping we have two doubles\n";
-	std::cout << DBdouble[0].PrintName() << '\n';
-	std::cout << DBdouble[1].PrintName() << '\n';
+	std::cout << "The list after removing elements :\n";
 
-	// fill algorithm
+	auto NewBegin = std::remove_if(NewList.begin(), NewList.end(), [](int x) {
+		return x > 3;
+		});
 
-	Fighter Krillin("Krillin", "Kame", "Kame1", "Kame2", "Kame3");
-	fill(DBdouble.begin(), DBdouble.end(), Krillin);
+	NewList.erase(NewBegin, NewList.end());
 
+		for (auto i : NewList)
+		{
+			std::cout << i << '\n';
 
-	std::cout << "\n\nChanging list with Krillins\n";
-	std::cout << "Now we have : \n";
-	std::cout << DBdouble[0].PrintName() << '\n';
-	std::cout << DBdouble[1].PrintName() << '\n';
+		}
+	// remove_if algorithm
 
 
-	
-	
-	
+
 }
