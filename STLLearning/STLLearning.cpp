9419ffcf52e7d58828abae770a8266af6d4d7c90
Ruslan Kolomenskiy <charly100@yandex.ru>
2025-05-12 09:00:41 +0300

    Lab8

--- STLLearning/STLLearning.cpp
+++ STLLearning/STLLearning.cpp
@@ -1,148 +1,168 @@
 
 
 #include <iostream>
+#include <memory>
 #include <string>
-#include <unordered_map>
-#include <map>
-#include <algorithm>
-#include <set>
-#include <list>
 #include <vector>
+#include <algorithm>
 
 
-
-
-int gen()
+void HeapSort(std::vector<int>& A)
 {
-	return rand()%12;
+	int Count = A.size();
+	std::vector<int>::iterator it = A.begin();
+	do
+	{
+		for (;it!=A.end()-1;++it)
+			if (*it > *(it + 1))
+			{
+				int temp = *(it + 1);
+				*(it + 1) = *it;
+				*it = temp;
+			}
+		it = A.begin();
+		Count--;
+	} while (Count!=0);
+}
+bool chkPremutation( std::vector<int>::const_iterator Str1,  std::vector<int>::const_iterator End1, std::vector<int>::const_iterator Str2, std::vector<int>::const_iterator End2)
+{
+	std::vector<int>::const_iterator temp1 = Str1;
+	std::vector<int>::const_iterator temp2 = Str2;
+
+
+	int Sum1 = 0;
+	int Sum2 = 0;
+	for (; Str1 != End1; Str1++)
+	{
+		Sum1 += *Str1;
+		//std::cout << Sum1;
+	}
+	for (; Str2 != End2; Str2++)
+	{
+		Sum2 += *Str2;
+	}
+	if (Sum1 != Sum2)
+	{
+		std::cout << "\nSum failed!\n";
+		return false;
+	}
+		
+
+	Str1 = temp1;
+	Str2 = temp2;
+
+	int count = 1;
+
+	for (; Str1 != End1; Str1++)
+	{
+		if (count == 1)
+		{
+			count = 0;
+			for (; Str2 != End2; ++Str2)
+			{
+				if (*Str1 == *Str2)
+					count++;
+
+			}
+		}
+		Str2 = temp2;
+	}
+
+	if (count == 1)
+		return true;
+	else
+		return false;
+}
+
+void printVector(const std::vector<int>& v)
+{
+	for (auto item : v)
+	{
+		std::cout << item << " ";
+
+	}
+	std::cout << '\n';
+}
+
+struct Arr
+{
+	Arr() : A(1.11), B(2), C(3.33) {}
+
+	double A;
+	int B;
+	float C;
+	
+	void print()
+	{
+		std::cout << "Struct elements = " << A << " " << B << " " << C << '\n';
+	}
 };
 
-
+struct S
+{
+	std::string m_message{ "Hello" };
+};
 
 int main()
 {
+	//uninitialized_memory
+	constexpr int n = 10;
 
-	//generate algorithm and partial_sort_copy
+	alignas(alignof(Arr)) double Mas[n * sizeof(Arr)];
 
-	std::cout << "My list is : \n";
+	auto first{ reinterpret_cast<Arr*>(Mas) };
+	auto last{ first + n };
 
-	std::vector<int> Vector1(10);
+	std::uninitialized_default_construct(first, last);
 
-	std::generate(Vector1.begin(), Vector1.end(), gen);
-
-	for (auto i : Vector1)
+	for (auto it{ first }; it != last; ++it)
 	{
-		std::cout << i << ' ';
-		
-	}
-
-	std::cout << '\n';
-
-	std::cout << "New list is : \n";
-
-	std::vector<int> Vector2(20);
-
-	std::generate(Vector2.begin(), Vector2.end(), gen);
-	
-	for (auto i : Vector2)
-	{
-		std::cout << i << ' ';
-
-	}
-	std::cout << '\n';
-	
-	std::partial_sort_copy(Vector1.begin(), Vector1.end(), Vector2.begin(), Vector2.end(), [](int x, int y) {
-		return  x>y;
-		});
-
-	std::cout << "After sort_copy : \n";
-
-	for (auto i : Vector2)
-	{
-		std::cout << i << ' ';
-
+		it->print();
 	}
 	
-	std::cout << '\n';
+	// heap
+
+	std::vector<int> Vec{ 6,3,867,4,234,54,-34,5777,987 };
+	printVector(Vec);
+
+	std::cout << "Vector is : \n";
+	std::make_heap(Vec.begin(), Vec.end());
+
+	std::cout << "Heap is : \n";
+	printVector(Vec);
+
+	std::pop_heap(Vec.begin(), Vec.end());
+
+	std::cout << "Heap after pop_heap : \n";
+	printVector(Vec);
+
+	//std::cout << std::is_heap(Vec.begin(), Vec.end());
+
+	std::push_heap(Vec.begin(), Vec.end());
+
+	std::cout << "Heap after push_heap : \n";
+	printVector(Vec);
+
+
+	std::cout << "Heap after sorting : \n";
+	HeapSort(Vec);
+	printVector(Vec);
+
+
+	//premutations
+
+	std::vector<int> X{ 1,2,3,7,6,5,4 };
+	std::vector<int> Y{ 7,4,5,3,6,2,1 };
+
 	
-	// lower_bound etc
-
-	std::sort(Vector1.begin(), Vector1.end());
-
-	auto Item = std::lower_bound(Vector1.begin(), Vector1.end(), 5);
-
-	std::cout << "list after sorting is : \n";
-
-	for (auto i : Vector1)
+	if (chkPremutation(X.begin(), X.end(), Y.begin(), Y.end()))
 	{
-		std::cout << i << ' ';
-
-	}
-
-	std::cout << "\nLooking for 5 \n";
-
-	if (*Item == 5)
-	{
-		std::cout << "there is a 5\n";
-
-		auto NextItem = std::upper_bound(Vector1.begin(), Vector1.end(),5);
-
-		std::cout << "the next after 5 is : " << *NextItem;
-
-	}
-	else
-		std::cout << "there is no 5\n";
-
-	
-	std::cout << "\nThe set is : \n";
-
-	std::set<int> seq = { 12,6573,321,3,78,92,45,65,56,123 };
-	for (auto i : seq) 
-	{
-		std::cout << i << " ";
-
-	}
-
-	std::cout << '\n'; 
-
-	std::cout << "\nUpper_bound of set 65 is : " << *seq.upper_bound(65);
-
-	std::cout << "\nLower_bound of set 66 is : " << *seq.lower_bound(66);
-
-
-	// includes, set difference
-
-	std::set<int> seq2 = { 78,92,45 };
-
-	std::cout << "\nThe second set is : \n";
-
-	for (auto i : seq2)
-	{
-		std::cout << i << " ";
-
-	}
-	std::cout << '\n';
-
-	std::cout << "Is the second a subsequence of the first?\n";
-	bool ItIs = std::includes(seq.begin(), seq.end(), seq2.begin(), seq2.end());
-	if (ItIs)
-	{
-		std::cout << "Yes\n";
+		std::cout << "\nIt is a premutation\n";
 	}
 	else
 	{
-		std::cout << "No\n";
+		std::cout << "\nIt is not a premutation\n";
 	}
 
-	std::set<int> seq3;
-	std::set_difference(seq.begin(), seq.end(), seq2.begin(), seq2.end(), std::inserter(seq3, seq3.begin()));
 
-	std::cout << "Now we copy all numbers from the first which are not in the second to the third : \n";
-
-	for (auto i : seq3)
-	{
-		std::cout << i << " ";
-
-	}
-	std::cout << '\n';
 }
